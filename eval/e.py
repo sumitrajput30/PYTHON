237b3f82ae676a05d44b351e5b8276da775e092b
sumit <sumitrajput88956@gmail.com>
2025-04-17 14:04:27 +0530

    Eval

---
+++ eval/e.py
@@ -0,0 +1,54 @@
+# x=eval(input("enter any no:"))
+# l=[]
+
+# for i in x:
+#     l.append(i**2)
+# print(l)
+
+
+# square root
+
+# x=eval(input("enter any no:"))
+# l=[]
+
+# for i in x:
+#     l.append(i**0.5)
+# print(l)
+
+# cube root
+
+# x=eval(input("enter any no:"))
+# l=[]
+
+# for i in x:
+#     l.append(i**0.33)
+# print(l)
+
+
+# type cast
+# x=eval(input("enter any no:"))
+# x=list(x)
+# l=[]
+
+# for i in x:
+#     l.append(i**2)
+
+# t=tuple(l)
+# print(t)
+
+
+# dict
+# x=eval(input("enter any no:"))
+# for i,j in x.items():
+#     print(x)
+
+# key
+x=eval(input("enter any no:"))
+for i in x.keys():
+    print(i)
+
+# value
+
+# x=eval(input("enter any no:"))
+# for i in x.values():
+#     print(i)
---
+++ eval/mymin.py
@@ -0,0 +1,21 @@
+# min
+l=eval(input("enter nay list"))
+minvalue=l[0]
+
+for i in l:
+    if i<minvalue:
+        minvalue=i
+
+print(minvalue)
+
+
+# max
+
+l1=eval(input("enter nay list"))
+maxvalue=l1[0]
+
+for i in l1:
+    if i>maxvalue:
+        maxvalue=i
+
+print(maxvalue)
