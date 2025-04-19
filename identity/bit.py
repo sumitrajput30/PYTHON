0ad9b1b46d0ee7f6bc2ff4c880a69581ecc199a5
sumit <sumitrajput88956@gmail.com>
2025-04-19 15:03:23 +0530

    Identity

---
+++ identity/bit.py
@@ -0,0 +1,28 @@
+# Bitwise operator
+
+# key:= And =$ , OR=| , Bitwise not = ~ , Bitwise x-or= ^ , left shift= <<, right shift= >>
+
+x=10
+print(bin(x))
+print(oct(x))
+print(hex(x))
+
+
+# binary
+x=0b10
+y=0B10
+print(x, y)
+
+# oct
+x=0o10
+y=0O10
+print(x, y)
+
+# hexa
+x=0
+
+print(x)
+
+x=10
+y=15
+print(x&y)
---
+++ identity/identy.py
@@ -0,0 +1,11 @@
+# identity operator
+# boolean 
+# it is compare from adress 
+x=10
+y=20
+
+print(x is y)
+
+print(x==y)
+
+print(x is not y)
