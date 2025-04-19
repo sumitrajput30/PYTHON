f785b8dc0fb1b8c53f8495e61a86d5aafd65df04
sumit <sumitrajput88956@gmail.com>
2025-04-19 15:01:45 +0530

    id

---
+++ id/Id.py
@@ -0,0 +1,45 @@
+# id
+x=10
+print(id(x))
+
+y=10
+print(id(y))
+
+z=10
+print(id(z))
+
+i=input("enter any name")
+print(i)
+
+k=input("enter any name")
+print(k)
+
+print(id(i),id(k))
+
+
+# maximum value
+s1='sumit'
+print(max(s1))
+
+# minimum value
+s1='sumit'
+print(min(s1))
+
+# ascci value to ascci
+
+a=input("enter any letter ")
+
+print(ord(a))
+
+# character to converted assci to character
+
+print(chr(65))
+
+# sum to use add the value
+
+l1=[10,20,30,40]
+print(sum(l1))
+
+# lengh
+
+print(len(l1))
