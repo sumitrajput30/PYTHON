680db7875a0c277aa9d0a2f1c8c6b21b10e6d8d7
sumit <sumitrajput88956@gmail.com>
2025-06-16 22:37:27 +0530

    Add two number

---
+++ Add two Number/add.py
@@ -0,0 +1,50 @@
+# a=10
+# b=20
+
+# # res=(a+b)
+# # print(res)
+# print(a+b)
+
+
+
+# # using user input 
+
+# A=int(input("enter any no "))
+# B=int(input("enter any no "))
+
+# print(A+B)
+
+# # float 
+# A=float(input("enter any no "))
+# B=float(input("enter any no "))
+
+# print(A+B)
+
+
+# using function 
+
+# def add(a,b):
+#     return a+b
+
+
+# a=eval(input("enter any no "))
+# b=eval(input("enter any no "))
+
+# res=(a+b)
+# print(res)
+
+# # using operator
+# import operator
+# print(operator.add(10,5))
+# print(operator.sub(10,5))
+# print(operator.mul(10,5))
+# print(operator.truediv(10,5))
+
+
+# using sum function 
+
+a=eval(input("enter any value "))
+# b=eval(input("enter any value "))
+
+
+print(sum(a))
