8571711b5ed3219a1a58abe5257952473faa5ca8
sumit <sumitrajput88956@gmail.com>
2025-06-16 22:38:58 +0530

    Find Max Number

---
+++ Find Max No/MAX.py
@@ -0,0 +1,43 @@
+# maximum number 
+
+# using max function 
+
+# a=int(input("enter any no "))
+# print(max(str(a)))
+
+# a = int(input("Enter any number: "))
+# print(max(str(a)))
+
+# # Using ternary operator 
+
+# a=int(input("enter any no"))
+# b=int(input("enter any no"))
+
+# print(a if a > b else b)
+
+# # using if else statement
+
+# a=int(input("enter any no: "))
+# b=int(input("enter any no: "))
+
+# if a>b:
+#     print(a)
+
+# else:
+#     print(b)
+
+
+# using sort()
+
+
+a = input("enter any no: ")
+a = list(a)
+a.sort()
+print(a[-1])
+
+
+a= int(input("enter any no: "))
+b= int(input("enter any no: "))
+c=list(str(a)+ str(b))
+c.sort()
+print(c[-1])
